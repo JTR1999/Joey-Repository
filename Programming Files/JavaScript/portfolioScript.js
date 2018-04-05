bc65ca9f9a135f1125bb35a3109b7a67ebee5174
Rose Joseph <310042509@cart.ads>
2018-04-05 09:54:45 -0700

    Finished Slideshow

--- Programming Files/JavaScript/portfolioScript.js
+++ Programming Files/JavaScript/portfolioScript.js
@@ -1,27 +1,20 @@
-var slideIndex = [1,1];
+var slideIndex = [1,1,1];
 var slideId = ["slideset1", "slideset2", "slideset3"]
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
 
 function showSlides(n, no) {
-    var i;
-    var x = document.getElementsByClassName(slideId[no]);
-    var dots = document.getElementsByClassName("dot");   
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    if (n > x.length) {slideIndex[no] = 1}    
-    if (n < 1) {slideIndex[no] = x.length}
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-    }
-    x[slideIndex[no]-1].style.display = "block";  
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
 }
