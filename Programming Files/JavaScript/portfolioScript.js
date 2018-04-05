d2321d1bfdbe073639bae1bb8999d58ec3a0b78b
Rose Joseph <310042509@cart.ads>
2018-04-05 10:20:20 -0700

    Final Commit 4-5

--- Programming Files/JavaScript/portfolioScript.js
+++ Programming Files/JavaScript/portfolioScript.js
@@ -1,3 +1,4 @@
+/*Slideshow JavaScript*/
 var slideIndex = [1,1,1];
 var slideId = ["slideset1", "slideset2", "slideset3"]
 showSlides(1, 0);
@@ -18,3 +19,11 @@ function showSlides(n, no) {
   }
   x[slideIndex[no]-1].style.display = "block";  
 }
+
+/*Contact Form JavaScript*/
+var email=('abc@abcmail.com');
+var subject = ('My Feedback');
+var cc = ('abc@abcmail.com');
+var bcc = ('abc@abcmail.com');
+var body = ('I am the body of the feedback that you are sending.');
+document.write( '<a href="mailto:' + email + '?subject=' + subject + '&cc=' + cc + '&bcc=' + bcc + '&body=' + body + '">' + 'Click here to send feedback' + '<' + '/a>');
