e78f125d4dc1147a44968657c6e2362981462648
Rose Joseph <310042509@cart.ads>
2018-04-05 09:08:40 -0700

    Slideshow Idea

--- Programming Files/JavaScript/portfolioScript.js
+++ Programming Files/JavaScript/portfolioScript.js
@@ -1,50 +1,27 @@
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 4000);
-var playing = true;
-var pauseButton = document.getElementById('pause');
-var next = document.getElementById('next');
-var previous = document.getElementById('previous')
+var slideIndex = [1,1];
+var slideId = ["slideset1", "slideset2", "slideset3"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-function nextSlide() {
-    goToSlide(currentSlide+1);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-function previousSlide() {
-    goToSlide(currentSlide-1);
-}
-
-function goToSlide(n) {
-    slides[currentSlide].className = 'slide'
-    currentSlide = (n+slides.length)%slides.length;
-    slides[currentSlide].className = 'slide showing'
-}
-function pauseSlideshow() {
-    pauseButton.innerHTML = 'Play';
-    playing = false;
-    clearInterval(slideInterval);
-}
-
-function playSlideshow() {
-    pauseButton.innerHTML = 'Pause'
-    playing = true;
-    slideInterval = setInterval(nextSlide, 4000);
-}
-
-pauseButton.onclick = function() {
-    if(playing) {
-        pauseSlideshow();
-    } else {
-        playSlideshow();
+function showSlides(n, no) {
+    var i;
+    var x = document.getElementsByClassName(slideId[no]);
+    var dots = document.getElementsByClassName("dot");   
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
+    if (n > x.length) {slideIndex[no] = 1}    
+    if (n < 1) {slideIndex[no] = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    x[slideIndex[no]-1].style.display = "block";  
 }
 
-next.onclick = function() {
-    pauseSlideshow();
-    nextSlide();
-}
-
-previous.onclick = function() {
-    pauseSlideshow();
-    previousSlide();
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
