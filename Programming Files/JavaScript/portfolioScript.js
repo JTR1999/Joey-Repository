31abd4ba7ff5cdc3f636dd04f28e7384f726ceea
Rose Joseph <310042509@cart.ads>
2018-04-06 09:33:56 -0700

    Form update incomplete

--- Programming Files/JavaScript/portfolioScript.js
+++ Programming Files/JavaScript/portfolioScript.js
@@ -19,3 +19,14 @@ function showSlides(n, no) {
   }
   x[slideIndex[no]-1].style.display = "block";  
 }
+
+/*Contact Form JavaScript*/
+function validateForm() {
+    var x = document.forms["contact"]["email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
+}
