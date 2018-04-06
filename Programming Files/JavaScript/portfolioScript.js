43584455f94b0c3a72c77b8a41b1b7f9a99c0c6e
Rose Joseph <310042509@cart.ads>
2018-04-06 08:20:37 -0700

    Contact Form start

--- Programming Files/JavaScript/portfolioScript.js
+++ Programming Files/JavaScript/portfolioScript.js
@@ -19,11 +19,3 @@ function showSlides(n, no) {
   }
   x[slideIndex[no]-1].style.display = "block";  
 }
-
-/*Contact Form JavaScript*/
-var email=('abc@abcmail.com');
-var subject = ('My Feedback');
-var cc = ('abc@abcmail.com');
-var bcc = ('abc@abcmail.com');
-var body = ('I am the body of the feedback that you are sending.');
-document.write( '<a href="mailto:' + email + '?subject=' + subject + '&cc=' + cc + '&bcc=' + bcc + '&body=' + body + '">' + 'Click here to send feedback' + '<' + '/a>');
